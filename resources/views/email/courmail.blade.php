0a00c923587466ec6ed9792f87461171bc5d34b1
MartinYve <orientmk714@gmail.com>
2022-05-24 23:26:17 +0200

    first commit

--- resources/views/email/courmail.blade.php
+++ resources/views/email/courmail.blade.php
@@ -9,11 +9,7 @@
 							<td align="center" valign="top">
 								<table  style="border:3px solid #3d8afd;" border="0" cellpadding="0" cellspacing="0" width="100%" class="tableCard" style="background-color:#fff;border-color:#e5e5e5;border-style:solid;border-width:0 1px 1px 1px;">
 									<tbody>
-										<tr>
-											<td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
-												<img alt="" border="0" src="{{($message->embed(public_path().'/images/logo.png'))}}"  width="250">
-											</td>
-										</tr>
+										
 										<tr>
 											<td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
 												<h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">Bonjour {{$etudiant['name']}}</h2>
